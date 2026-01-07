75a7571dbfc68ab83e0f4ad41cccfb2fb1625d9a
ludovc <ludovcasu@gmail.com>
2026-01-07 15:15:28 +0100

    norm

--- src/cub3d.c
+++ src/cub3d.c
@@ -16,10 +16,8 @@ static void	initialization(t_game *game)
 {
 	game->mlx = mlx_init();
 	game->win = mlx_new_window(game->mlx, WIDTH, HEIGHT, "CUB3D");
-	
 	init_image(game);
 	game->state = MENU;
-
 	game->keys.w_pressed = 0;
 	game->keys.a_pressed = 0;
 	game->keys.s_pressed = 0;
@@ -36,33 +34,32 @@ void	free_all(t_game *game)
 		mlx_destroy_image(game->mlx, game->img.img);
 	if (game->menu_img.img)
 		mlx_destroy_image(game->mlx, game->menu_img.img);
-	if (game->txtrs.n_wall.img) mlx_destroy_image(game->mlx, game->txtrs.n_wall.img);
-	if (game->txtrs.s_wall.img) mlx_destroy_image(game->mlx, game->txtrs.s_wall.img);
-	if (game->txtrs.e_wall.img) mlx_destroy_image(game->mlx, game->txtrs.e_wall.img);
-	if (game->txtrs.w_wall.img) mlx_destroy_image(game->mlx, game->txtrs.w_wall.img);
+	if (game->txtrs.n_wall.img)
+		mlx_destroy_image(game->mlx, game->txtrs.n_wall.img);
+	if (game->txtrs.s_wall.img)
+		mlx_destroy_image(game->mlx, game->txtrs.s_wall.img);
+	if (game->txtrs.e_wall.img)
+		mlx_destroy_image(game->mlx, game->txtrs.e_wall.img);
+	if (game->txtrs.w_wall.img)
+		mlx_destroy_image(game->mlx, game->txtrs.w_wall.img);
 	free_strarr(game->map);
 	mlx_destroy_window(game->mlx, game->win);
 	mlx_destroy_display(game->mlx);
-
 	free(game->mlx);
 }
 
-
-int	main()
+int	main(void)
 {
-	t_game game;
+	t_game	game;
 
 	parsing(&game);
 	initialization(&game);
 	load_textures(&game);
-	spawn_player(&game); // puff!
-	
+	spawn_player(&game);
 	mlx_hook(game.win, 17, 0, close_window, &game);
-	mlx_hook(game.win, 2, 1L<<0, handle_keypress, &game);
-	mlx_hook(game.win, 3, 1L<<1, handle_keyrelease, &game); 
+	mlx_hook(game.win, 2, 1L << 0, handle_keypress, &game);
+	mlx_hook(game.win, 3, 1L << 1, handle_keyrelease, &game);
 	mlx_loop_hook(game.mlx, game_loop, &game);
 	mlx_loop(game.mlx);
-
 	free_all(&game);
 }
-
