64338e1158c221fe8a841e94ffdb4b0f3977b5a0
ludovc <ludovcasu@gmail.com>
2026-01-14 15:59:54 +0100

    norm

--- src/init.c
+++ src/init.c
@@ -28,31 +28,44 @@ static void	load_menu_image(t_game *game)
 	int		w;
 	int		h;
 
-	game->menu_img.img = mlx_xpm_file_to_image(game->mlx, "xpm/menu.xpm", &w, &h);
+	game->menu_img.img = mlx_xpm_file_to_image(game->mlx,
+			"xpm/menu.xpm", &w, &h);
 	game->menu_img.addr = mlx_get_data_addr(game->menu_img.img,
-			&game->menu_img.bpp, &game->menu_img.line_length, &game->menu_img.endian);
+			&game->menu_img.bpp, &game->menu_img.line_length,
+			&game->menu_img.endian);
 }
 
-int	load_textures(t_game *game)
+static int	load_wall_texture(t_game *game, t_img *img, char *path)
 {
 	int		w;
 	int		h;
 
-	game->txtrs.n_wall.img = mlx_xpm_file_to_image(game->mlx, NORTH_WALL, &w, &h);
-	game->txtrs.n_wall.addr = mlx_get_data_addr(game->txtrs.n_wall.img,
-			&game->txtrs.n_wall.bpp, &game->txtrs.n_wall.line_length, &game->txtrs.n_wall.endian);
-	game->txtrs.s_wall.img = mlx_xpm_file_to_image(game->mlx, SOUTH_WALL, &w, &h);
-	game->txtrs.s_wall.addr = mlx_get_data_addr(game->txtrs.s_wall.img,
-			&game->txtrs.s_wall.bpp, &game->txtrs.s_wall.line_length, &game->txtrs.s_wall.endian);
-	game->txtrs.e_wall.img = mlx_xpm_file_to_image(game->mlx, EAST_WALL, &w, &h);
-	game->txtrs.e_wall.addr = mlx_get_data_addr(game->txtrs.e_wall.img,
-			&game->txtrs.e_wall.bpp, &game->txtrs.e_wall.line_length, &game->txtrs.e_wall.endian);
-	game->txtrs.w_wall.img = mlx_xpm_file_to_image(game->mlx, WEST_WALL, &w, &h);
-	game->txtrs.w_wall.addr = mlx_get_data_addr(game->txtrs.w_wall.img,
-			&game->txtrs.w_wall.bpp, &game->txtrs.w_wall.line_length, &game->txtrs.w_wall.endian);
-	load_menu_image(game);
-	if (!game->txtrs.n_wall.img || !game->txtrs.s_wall.img || !game->txtrs.e_wall.img || !game->txtrs.w_wall.img)
+	img->img = mlx_xpm_file_to_image(game->mlx, path, &w, &h);
+	if (!img->img)
 		return (0);
+	img->addr = mlx_get_data_addr(img->img, &img->bpp,
+			&img->line_length, &img->endian);
+	return (img->addr != NULL);
+}
+
+static int	load_all_wall_textures(t_game *game)
+{
+	if (!load_wall_texture(game, &game->txtrs.n_wall, NORTH_WALL))
+		return (0);
+	if (!load_wall_texture(game, &game->txtrs.s_wall, SOUTH_WALL))
+		return (0);
+	if (!load_wall_texture(game, &game->txtrs.e_wall, EAST_WALL))
+		return (0);
+	if (!load_wall_texture(game, &game->txtrs.w_wall, WEST_WALL))
+		return (0);
+	return (1);
+}
+
+int	load_textures(t_game *game)
+{
+	if (!load_all_wall_textures(game))
+		return (0);
+	load_menu_image(game);
 	return (1);
 }
 
